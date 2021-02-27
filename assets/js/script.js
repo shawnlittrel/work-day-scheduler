0ac831c2eeb96ecda0e2694536a713ae80cb7484
Shawn Littrel <shawnlittrel@protonmail.com>
2021-02-26 19:52:31 -0700

    added basic layout and files needed to complete

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//insert current day into header
+
+//put date picker into header?  will always load previous days tasks
+
+//insert text-boxes into each hour of the day
+
+//save button for each text box
+
+//past hours are grey, current hour is pale yellow, future hours are pale green
+
+//save all tasks save button click
